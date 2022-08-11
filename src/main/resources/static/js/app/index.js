c2de01579fb7d50d81e33fada4f8c04438c03c61
preetysis1234 <preetysis1234@gmail.com>
2022-08-11 15:33:32 +0900

    Day3 소스 업로드 Hello 예제소스 만듬

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,50 @@
+var main = {
+    init : function() {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save : function () {
+        var data = {
+                title: $('#title').val(),
+                author : $('#author').val(),
+                content : $('#content').val()
+    };
+    $.ajax({
+            type:'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function(){ //성공
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function(request, status,error){    //실패
+           alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+
+           "error:"+error);
+        });
+    },
+    update : function(){
+        var data = {
+            title: $('#title').val();
+            content: $('#content').val();
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type : 'PUT',
+            url : '/api/v1/posts/'+id,
+            dataType : 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function(){
+            alert('글이 수정되었습니다.');
+            window.location.href='/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+            });
+    }
+};
+main.init();    //이게 실행이 됐기때문에 위쪽 function이 동작하는 것이다.
